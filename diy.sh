d8fb645483c1813754a1d027d195cd1a2035baa8
xwdj <40928432+1787755656@users.noreply.github.com>
2020-03-02 16:39:48 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,3 +7,4 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/OpenWrt/xwit/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh 
