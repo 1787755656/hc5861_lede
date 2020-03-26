38b21503f1e9362608f59b7551e0a1c5a208dd72
xwdj <40928432+1787755656@users.noreply.github.com>
2020-03-26 16:03:43 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,7 +7,7 @@
 #=================================================
 # Modify default IP
 #git clone https://github.com/jerrykuku/luci-theme-argon package/
-#sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 #sed -i 's/OpenWrt/xwit/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh 
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile 
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile 
 
