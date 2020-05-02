1cea22518821cb6f71e5e1888f071ecab48bcc8c
xwdj <40928432+1787755656@users.noreply.github.com>
2020-05-03 00:02:31 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -11,3 +11,8 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 #sed -i 's/OpenWrt/xwit/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh 
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile 
 
+#!/bin/bash
+mkdir package/base-files/files/config
+echo 0xDEADBEEF > package/base-files/files/config/google_fu_mode
+echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
+./scripts/feeds update -a && ./scripts/feeds install -a
